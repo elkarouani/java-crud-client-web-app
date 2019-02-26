4a7a30a771ec513dfc4bf72190ad5ce541ca24fd
KDragon <elkarouani@gmail.com>
2019-02-26 17:16:04 +0100

    Task: editing on addClient method on client dao class

--- src/dao/ClientDAO.java
+++ src/dao/ClientDAO.java
@@ -1,19 +1,26 @@
 package dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
 import model.Client;
 
 public class ClientDAO {
-	@PersistenceContext
+	@PersistenceContext(unitName = "GestionCommandes")
 	EntityManager em;
 	public int addClient(Client cl) {
+//		Créer une instance em et la stocker dans la session
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("GestionCommandes");
+		em = emf.createEntityManager();
 //		Persister cl
+		EntityTransaction t = em.getTransaction();
+		t.begin();
 		em.persist(cl);
+		t.commit();
 //		Actualiser
-		em.flush();
-		
 		return 1;
 	}
 	

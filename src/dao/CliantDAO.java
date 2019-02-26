7d960eb715ce2ff7bd2d972e55ce52a8f91c7453
KDragon <elkarouani@gmail.com>
2019-02-26 16:45:50 +0100

    Task: adding addClient method on client dao class

--- src/dao/CliantDAO.java
+++ src/dao/CliantDAO.java
@@ -1,5 +1,15 @@
 package dao;
 
-public class CliantDAO {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import model.Client;
+
+public class CliantDAO {
+	@PersistenceContext ()
+	EntityManager em;
+	public int addClient(Client cl) {
+		return 1;
+	}
+	
 }
